a4fca01fb1f5ce98fcb1bc736edeeac00097b334
Daniel Migales <daniel.migales.puertas@gmail.com>
2020-03-04 21:47:19 +0100

    v.1.3

--- app/src/main/java/com/example/proyectonavigation/ChangeProfileActivity.java
+++ app/src/main/java/com/example/proyectonavigation/ChangeProfileActivity.java
@@ -1,16 +1,148 @@
 package com.example.proyectonavigation;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class ChangeProfileActivity extends AppCompatActivity {
 
-    @Override
+    private ImageView photo;
+    private Button takePicture;
+    private Bitmap imageBitmap;
+    private String UPLOAD_URL = "https://proyectogrupod.000webhostapp.com/register_php/uploadPhoto.php";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String KEY_PHOTO = "photo";
+    private String KEY_NAME = "name";
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_profile);
-
         getSupportActionBar().hide();
+
+        requestCameraPermission();
+
+        photo = findViewById(R.id.imageViewPhoto);
+
+        takePicture = findViewById(R.id.buttonPhoto);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+    //SOLICITAR PERMISOS
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                REQUEST_IMAGE_CAPTURE);
+    }
+
+    //EL METODO FALLA PORQUE DENIEGA EL PERMISO A LA CAMARA. RECORDATORIO DE REVISAR PERMISOS
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //COLOCA LA FOTO EN EL IMAGEVIEW
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            photo.setImageBitmap(imageBitmap); //COLOCA LA IMAGEN EN EL IMAGEVIEW
+
+            uploadImage();
+        }
+    }
+
+    //CODIGO PARA SUBIR IMAGEN A BASE DE DATOS
+
+    public String getStringImagen(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage() {
+        //MUESTRA UN DIALOGO DE PROGRESO
+        final ProgressDialog loading = ProgressDialog.show(this, "Uploading...", "uploading...", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+                        //Mostrando el mensaje de la respuesta
+                        Toast.makeText(ChangeProfileActivity.this, s, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+                        //Showing toast
+                        Toast.makeText(ChangeProfileActivity.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Convertir bits a cadena
+                String photo = getStringImagen(imageBitmap);
+
+                //Creación de parámetros
+                Map<String, String> params = new Hashtable<String, String>();
+
+                //Agregando de parámetros
+                params.put(KEY_PHOTO, photo);
+               // params.put(KEY_NAME, name);
+
+                //Parámetros de retorno
+                return params;
+            }
+        };
+
+        //Creación de una cola de solicitudes
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Agregar solicitud a la cola
+        requestQueue.add(stringRequest);
+    }
+
+
 }
+
+
+
+
